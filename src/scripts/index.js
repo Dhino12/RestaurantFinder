900c4ec3948870263ef052589c7edb515c1c5dc8
dhino12 <dhinorahmad0@gmail.com>
2022-09-06 10:18:20 +0700

    starter build this project

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import data from '../DATA.json'
+console.log(data);
+
+console.log('Hello Coders! :)');
+
+// const data = await fetch('')
