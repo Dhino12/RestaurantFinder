427c900a72f085c88d04318a2fc6092b815ed96c
dhino12 <dhinorahmad0@gmail.com>
2022-09-06 10:27:03 +0700

    remove templating

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,5 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
-import data from '../DATA.json'
-console.log(data);
 
 console.log('Hello Coders! :)');
 
